dabf871b3663a9ec788225b75b394057aaceff55
tpaphysics <physics.thiago@gmail.com>
2022-05-12 02:11:24 -0300

    chore: adição do chakra-ui com pacotes, @chakra-ui/react @emotion/react@^11 @emotion/styled@^11 framer-motion@^6

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,5 +2,7 @@ module.exports = {
   extends: "airbnb-typescript-prettier",
   rules: {
     "react/react-in-jsx-scope": "off",
+    "@typescript-eslint/no-shadow": "off",
+    "@typescript-eslint/no-non-null-assertion": "off",
   },
 };
